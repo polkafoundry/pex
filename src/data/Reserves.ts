2f7ef278cbb81a962517bc8550f0fdad35b8ef62
chuoikhungbo <quanvantruong73@gmail.cp>
2021-03-29 13:31:58 +0700

    fix fixed pair address

--- src/data/Reserves.ts
+++ src/data/Reserves.ts
@@ -1,4 +1,4 @@
-import { TokenAmount, Pair, Currency } from '@uniswap/sdk'
+import { TokenAmount, Pair, Currency, Token } from '@uniswap/sdk'
 import { useMemo } from 'react'
 import { abi as IUniswapV2PairABI } from '@uniswap/v2-core/build/IUniswapV2Pair.json'
 import { Interface } from '@ethersproject/abi'
@@ -6,7 +6,14 @@ import { useActiveWeb3React } from '../hooks'
 
 import { useMultipleContractSingleData } from '../state/multicall/hooks'
 import { wrappedCurrency } from '../utils/wrappedCurrency'
-import {ERC20, fixedPairAddress } from "../constants"
+import {
+  // ERC20,
+  FACTORY_ADDRESS,
+  // fixedPairAddress,
+  INIT_CODE_HASH
+} from "../constants"
+import { pack, keccak256 } from '@ethersproject/solidity'
+import { getCreate2Address } from '@ethersproject/address'
 
 const PAIR_INTERFACE = new Interface(IUniswapV2PairABI)
 
@@ -15,6 +22,15 @@ export enum PairState {
   NOT_EXISTS,
   EXISTS,
   INVALID
+}
+
+const computePairAddress = ({tokenA, tokenB}: { tokenA: Token, tokenB: Token }): string => {
+  const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA] // does safety checks
+  return getCreate2Address(
+    FACTORY_ADDRESS,
+    keccak256(['bytes'], [pack(['address', 'address'], [token0.address, token1.address])]),
+    INIT_CODE_HASH
+  )
 }
 
 export function usePairs(currencies: [Currency | undefined, Currency | undefined][]): [PairState, Pair | null][] {
@@ -31,13 +47,14 @@ export function usePairs(currencies: [Currency | undefined, Currency | undefined
   const pairAddresses = useMemo(
     () =>
       tokens.map(([tokenA, tokenB]) => {
-        if((tokenA?.address  === ERC20[0]?.address  && tokenB?.address  === ERC20[1]?.address )
-            || (tokenA?.address  === ERC20[1]?.address  && tokenB?.address  === ERC20[0]?.address ))
-          return fixedPairAddress
-        return tokenA && tokenB && !tokenA.equals(tokenB) ? Pair.getAddress(tokenA, tokenB) : undefined
+        // if((tokenA?.address  === ERC20[0]?.address  && tokenB?.address  === ERC20[1]?.address )
+        //     || (tokenA?.address  === ERC20[1]?.address  && tokenB?.address  === ERC20[0]?.address ))
+        //   return fixedPairAddress
+        return tokenA && tokenB && !tokenA.equals(tokenB) ? computePairAddress({tokenA, tokenB}) : undefined
       }),
     [tokens]
   )
+
   const results = useMultipleContractSingleData(pairAddresses, PAIR_INTERFACE, 'getReserves')
 
   return useMemo(() => {
