1772d9000e6b70c6c3a9a0e79b1ca005847aad2b
chuoikhungbo <quanvantruong73@gmail.cp>
2021-04-05 17:15:39 +0700

    fix typo

--- src/components/warning/WarningModal.tsx
+++ src/components/warning/WarningModal.tsx
@@ -64,7 +64,7 @@ export default function WarningModal({isOpen, onClose}: ModalProps) {
         </CloseIcon>
         <Title>
           <Paragraph>PEX is an Uniswap clone on <Link href="https://polkafoundry.com/" title="PolkaFoundry"/> testnet.</Paragraph>
-          <Paragraph style={{fontSize: '16px'}}>Currently, it support only ETH-PKF pair.</Paragraph>
+          <Paragraph style={{fontSize: '16px'}}>Currently, it supports only ETH-PKF pair.</Paragraph>
         </Title>
         <Subtitle>
           You will have to setup MetaMask's Custom RPC before using Polkex. From the MetaMask's 'Networks' dropdown, choose 'Custom RPC' and input the following info:
@@ -74,7 +74,7 @@ export default function WarningModal({isOpen, onClose}: ModalProps) {
           <li>RPC URL: https://halongbay.polkafoundry.com</li>
           <li>Chain ID: 13</li>
         </ul>
-        <Paragraph style={{fontSize: '12px', color: '#747474'}}>Please refer <Link href="" title="here"/> for introduction and further instruction. To get some PKF funds for your trial, please click <Link href="t.me/pkf_faucet_bot" title="here"/>.</Paragraph>
+        <Paragraph style={{fontSize: '12px', color: '#747474'}}>Please refer <Link href="" title="here"/> for introduction and further instruction. To get some PKF funds for your trial, please click <Link href="https://t.me/pkf_faucet_bot" title="here"/>.</Paragraph>
       </Wrapper>
     </Modal>
   )
